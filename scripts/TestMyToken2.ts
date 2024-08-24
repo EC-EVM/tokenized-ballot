0afd8294150ba574df57d3a2574468b643c7f9a3
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-08-23 18:11:15 -0700

    🐙 changes on TestMyToken2

--- scripts/TestMyToken2.ts
+++ scripts/TestMyToken2.ts
@@ -11,23 +11,31 @@ async function main() {
   console.log(`Token contract deployed at ${contract.address}\n`);
   // Token contract deployed at 0x5fbdb2315678afecb367f032d93f642f64180aa3
 
-    // try to mint some tokens graver2
-    const mintTx = await contract.write.mint([acc1.account.address, MINT_VALUE]);
-    await publicClient.waitForTransactionReceipt({ hash: mintTx });
+  // try to mint some tokens graver2
+  const mintTx = await contract.write.mint([acc1.account.address, MINT_VALUE]);
+  await publicClient.waitForTransactionReceipt({ hash: mintTx });
     console.log(
       `Minted ${MINT_VALUE.toString()} decimal units to account ${
         acc1.account.address
       }\n`
     // Minted 100 decimal units to account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8
-    );
-    const balanceBN = await contract.read.balanceOf([acc1.account.address]);
+  );
+  const balanceBN = await contract.read.balanceOf([acc1.account.address]);
     console.log(
       `Account ${
         acc1.account.address
       } has ${balanceBN.toString()} decimal units of MyToken\n`
     // Account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 has 100 decimal units of MyToken
-    );
+  );
 
+  // checking voting power
+  const votes = await contract.read.getVotes([acc1.account.address]);
+  console.log(
+  `Account ${
+    acc1.account.address
+  } has ${votes.toString()} units of voting power before self delegating\n`
+  // Account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 has 0 units of voting power before self delegating
+  ); 
 }
 
 main().catch((err) => {
