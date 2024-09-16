b1edcf8b5ddda5d680978762b543ef7564d2dd17
Christopher Mayfield <christopher.j.mayfield@gmail.com>
2024-09-15 19:43:15 -0700

    add update contract

---
+++ contracts/update.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Test {
+
+    int myValue = 55;
+
+
+    function myfunc(uint aNumber) public returns (uint) {
+        return aNumber;
+    }
+}
